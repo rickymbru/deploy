2c846e003a6d2110c9c58997b8527a18495cb784
Ricky Menezes Bruggemann <ricky@cedae.com.br>
2019-11-07 12:00:18 -0300

    first commit

---
+++ deploymetrus.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+#Variaveis
+APP=metrus
+export DOMAIN_HOME=/u01/domains/metrus
+DEPLOY="/var/weblogic-external-data/$APP/deploy"
+CONTROl="/home/oracle/version$APP.txt"
+LOG=$DEPLOY"/log-"$APP".txt"
+MOUNT=/var/weblogic-external-data
+USERCONFIG='/home/oracle/oracle-WebLogicConfig.properties'
+USERKEY='/home/oracle/oracle-WebLogicKey.properties'
+PATH=$PATH:$HOME/.local/bin:$HOME/bin
+export CLASSPATH='/u01/middleware/fmw12c/wlserver/server/lib/weblogic.jar'
+export MW_HOME=/u01/middleware/wls12c
+export WLS_HOME=$MW_HOME/wlserver
+export WL_HOME=$WLS_HOME
+export JAVA_HOME=/u01/middleware/jdk
+export PATH=$JAVA_HOME/bin:$PATH
+export URL=`grep ADMIN_URL $DOMAIN_HOME/bin/stopWebLogic.sh  | grep t3  | cut -d '=' -f2 | sed -e 's/"//g'`
+PID=/home/oracle/DeployMetrus.pid
+
+#verifica se o Deploy já está em andamento
+if [ -f "$PID" ]; then
+        echo `date` - Deploy já em andamento, saindo... >>$LOG
+        exit 1
+else touch $PID		
+fi
+
+# Verifica se o /weblogic-external-data está montado.
+if ! mountpoint -q $MOUNT;
+then
+        exit 1
+fi
+
+#Seleciona o arquivo mais recente em $DEPLOY
+unset NEWEST
+for filename in $DEPLOY/*.ear; do
+    if [ -f "$filename" ] && [ "$filename" -nt "$NEWEST" ]; then
+        NEWEST=$filename
+    fi
+done
+
+#Validação do formato do arquivo ear, se possui $APP-versão
+if [[ ! $NEWEST =~ $APP ]]
+then
+   "Arquivo ear fora do formato: "$APP"_versão"
+   exit 1
+fi
+
+NEWESTDATA=`date --reference "$NEWEST" +%Y%m%d%H%M%S`
+#echo NEWEST: $NEWESTDATA
+
+CONTROLDATA=`cat "$CONTROl"`
+#echo CONTROL: $CONTROLDATA
+
+if [ "$NEWESTDATA" != "$CONTROLDATA" ]; then
+		if [[ "$NEWESTDATA" > "$CONTROLDATA" ]]; then
+			#echo "Fazendo o Redeploy..."
+			#echo $NEWEST
+   
+			java -cp $CLASSPATH weblogic.Deployer -adminurl $URL -userconfigfile $USERCONFIG -userkeyfile $USERKEY -redeploy -name $APP -source $NEWEST -usenonexclusivelock >>$LOG && echo $NEWESTDATA > $CONTROl
+   
+		fi
+fi
+rm $PID	   
+
+
