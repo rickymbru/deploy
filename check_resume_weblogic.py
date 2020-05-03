a80720229ee5684dc2c8b5be3b36259d69c8b7e7
Ricky <rickymbru@gmail.com>
2020-05-03 15:49:07 -0300

    Check and resume Weblogic console

---
+++ check_resume_weblogic.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python2.7
+import signal
+import time
+import os
+import subprocess
+import smtplib
+from contextlib import contextmanager
+
+class TimeoutException(Exception): pass
+
+@contextmanager
+def time_limit(seconds):
+    def signal_handler(signum, frame):
+        raise TimeoutException("Timed out!")
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(seconds)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
+
+def criaconnect():
+    os.system('echo "connect()">connect-default.py')        
+
+def sendemail(from_addr, to_addr_list, cc_addr_list,
+              subject, message,
+              smtpserver='10.10.0.128:25'):
+    header  = 'From: %s' % from_addr+'\n'
+    header += 'To: %s' % ','.join(to_addr_list)+'\n'
+    header += 'Cc: %s' % ','.join(cc_addr_list)+'\n'
+    header += 'Subject: %s' % subject+'\n'
+    message = header + message
+ 
+    server = smtplib.SMTP(smtpserver)
+    problems = server.sendmail(from_addr, to_addr_list, message)
+    server.quit()
+
+def startweblogic():
+    DOMAIN_HOME = os.environ.get('DOMAIN_HOME')
+    STARTWEBLOGIC='nohup '+ DOMAIN_HOME +'/bin/startWebLogic.sh &'
+    os.system(STARTWEBLOGIC)
+
+def killweblogic(PORT):
+    COMMAND_PID='netstat -ltnp 2>/dev/null| grep -w '+ PORT +' | grep :1 | awk \'{print $7}\' | cut -d \'/\' -f 1'
+    PID = arch = subprocess.check_output(COMMAND_PID, shell=True);
+    COMMAND_KILL='kill -9 '+PID    
+    os.system(COMMAND_KILL)       
+
+def connect(WLST):
+    criaconnect()    
+    if os.system(WLST+' connect-default.py') == 0:
+        message='Weblogic Connect !'
+        print(message)
+        sendemail(from_addr    = 'fagundes@cedae.com.br', 
+          to_addr_list = ['ricky@cedae.com.br'],
+          cc_addr_list = [], 
+          subject      = message, 
+          message      = message)
+    else:
+        message='Weblogic not Connect ! Starting...'        
+        print(message)
+        sendemail(from_addr    = 'fagundes@cedae.com.br', 
+          to_addr_list = ['ricky@cedae.com.br'],
+          cc_addr_list = [], 
+          subject      = message, 
+          message      = message)
+        startweblogic()
+
+# Variaveis
+WLST = '/u01/middleware/wls12c/oracle_common/common/bin/wlst.sh'
+PORT = '7001'
+
+try:
+    with time_limit(20):        
+        connect(WLST)
+except TimeoutException as e:
+    message='Weblogic Timout! Killing and starting...'
+    print(message)
+    sendemail(from_addr    = 'fagundes@cedae.com.br', 
+        to_addr_list = ['ricky@cedae.com.br'],
+        cc_addr_list = [], 
+        subject      = message, 
+        message      = message)    
+    killweblogic(PORT)
+    startweblogic() 
